b0fa1e4bfd25120f85ac22d7f4b643d8053a580b
Awea <aweaoftheworld@gmail.com>
2015-05-17 01:34:29 +0200

    configurable path to rss file for ReivaxHttp

--- apps/reivax_http/web/controllers/page_controller.ex
+++ apps/reivax_http/web/controllers/page_controller.ex
@@ -11,7 +11,11 @@ defmodule ReivaxHttp.PageController do
   end
 
   defp rss do
-    File.read("/home/m/code/elixir/twitter/dev.rss")
+    File.read(rss_file_path)
     |> elem 1
+  end
+
+  defp rss_file_path do
+    Application.get_env(:reivax_http, :rss_file_path)
   end
 end
